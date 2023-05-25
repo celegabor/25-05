0c3338d3a6f77a2a2002a388db0af82e6991126b
celegabor <127986056+celegabor@users.noreply.github.com>
2023-05-25 21:36:11 +0200

    modulo2

---
+++ js.js
@@ -0,0 +1,49 @@
+// elenco datatype
+
+// questo serve per inserire solo numeri in cifre
+let Numeri= 8
+
+// questo serve e viene letto solo a parole 
+let Stringa= 'stringa'
+
+// questo non è mai stato dichiarato quindi non ha mai avuto qualche informazione
+let Undefined
+
+// questo viene dichiarato, ma viene dichiarato vuoto senza nessun valore
+let Null= null
+
+// queste sono 3 possibilità per verificare se sono vere o false
+let Boolean= 'And/Not/Or'
+
+
+let BigInt='?'
+let Symbol= '?'
+
+let primoNumero= 12
+let secondoNumero= 20
+let risultato= primoNumero + secondoNumero
+
+console.log('il tuo risultato è: ', + risultato)
+
+
+
+let x= 12
+let nome= 'Celestin'
+
+
+console.log('calcolo 4-x: ', 4 - x)
+
+let identità={
+    nome:'Celestin',
+    cognome: 'Gabor',
+    hobby: 'seguire le lezioni di Epicode'
+}
+
+
+let name1= 'john'
+let name2= 'John'
+
+console.log('sono diversi? ', name1 === name2)
+
+
+console.log('2, sono diversi? ', name1 !== name2)
